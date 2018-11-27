503efb0f2303fb81df6bdf85dbb380bf63515e75
Kazik Pogoda <morisil@xemantic.com>
2018-11-27 23:31:29 +0100

    codepen specific JS

---
+++ js/codepen.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  var body = document.querySelector('body');
+  var canvas = document.querySelector('canvas');
+  var tube = initializeTube(display);
+  var demo = {
+    setUpContext: (typeof setUpContext === 'function') ? setUpContext : function(context) { return context },
+    updateContext: (typeof updateContext === 'function') ? updateContext : function(context) { return context },
+    getColor: getColor
+  };
+  window.onresize = function() {
+    tube.resize();
+  };
+  tube.startCathodeRay(demo);
+};
